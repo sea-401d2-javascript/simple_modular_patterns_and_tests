80363a3f682c2a8f090ef44b91cd2e85f0984a03
Thom Nienow <elstad.nienow@gmail.com>
2016-03-01 09:26:25 -0800

    added folder with my name

---
+++ nienowt-feb29/test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+var greeting = require(__dirname + '/../lib/greet.js');
+var greet = greeting('thorn');
+
+
+describe('greeting', function(){
+  it('greeting should return "hello + name"', function(){
+    expect(greet).to.eql('hello thorn');
+  });
+});
