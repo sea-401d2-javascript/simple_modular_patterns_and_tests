718cea335ecd3d24c0f5399a431111672f08c40b
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-01 09:27:10 -0800

    added gitignore

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var expect = require('chai').expect;
-var program = require('/../lib/tags.js');
+var program = require('../lib/tags.js');
 
 describe('greet', function() {
   it('should return hello plus the name parameter', function(){
-    expect(greet('ted').to.equal('hello ted'););
+    var testGreet = greet('ted');
+    expect(testGreet).to.equal('hello ted');
   });
 });
