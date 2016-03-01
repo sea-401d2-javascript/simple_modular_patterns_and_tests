380cabf00204a80dcfda7a3b271528d1e568ed3b
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-01 09:17:29 -0800

    wrote initial test

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -2,3 +2,9 @@
 
 var expect = require('chai').expect;
 var program = require('/../lib/tags.js');
+
+describe('greet', function() {
+  it('should return hello plus the name parameter', function(){
+    expect(greet('ted').to.equal('hello ted'););
+  });
+});
