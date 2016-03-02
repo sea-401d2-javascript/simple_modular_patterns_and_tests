ab73dbd7cf7d725779e8a291b76f0ad1e413fb3d
Frazier Mork <fraziermork@gmail.com>
2016-03-02 10:02:58 -0800

    switched from default .eslintrc file to reading a file using gulp and plugging it into the options for eslint

--- frazier/gulpfile.js
+++ frazier/gulpfile.js
@@ -1,19 +1,26 @@
 'use strict';
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var jshint = require('gulp-jshint');
+var fs = require('fs');
 var mocha = require('gulp-mocha');
+
 
 var paths = ['lib/*.js', 'test/*.js'];
 
 gulp.task('eslint', function(){
-  console.log('eslint called');
   return gulp.src(paths)
-  .pipe(eslint())
+  .pipe(eslint(fs.readFileSync('gulp-eslintrc')))
   .pipe(eslint.format());
 });
 
+//no longer being used because we are using eslint instead
+gulp.task('jshint', function(){
+  return gulp.src(paths)
+  .pipe(jshint());
+});
+
 gulp.task('test', ['eslint'], function(){
-  console.log('test called');
   return gulp.src('test/greetTest.js')
   .pipe(mocha());
 });
